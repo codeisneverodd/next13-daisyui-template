3e3ae26e02e9676e17ecc2d001f4916190b07968
codeisneverodd <codeisneverodd@gmail.com>
2023-06-28 23:15:06 +0900

    feat: add font pretendard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,9 @@ module.exports = {
     "./node_modules/react-daisyui/dist/**/*.js",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    fontFamily: {
+      extends: {
+        sans: ["var(--font-pretendard)"],
       },
     },
   },
